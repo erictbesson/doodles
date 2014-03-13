1e8c30808b62a04f6b4be3c604986586f841c5d4
erictbesson <AlekianKnight@hotmail.com>
2014-03-12 21:31:40 -0400

    3-11 bug fixes and more functionality

--- RPG Code.py
+++ RPG Code.py
@@ -16,19 +16,25 @@ Bee = {'name': 'Bee', 'speed': 20}
 
 test_Participants = [Kiro, Generic_Enemy, Bee]
 
+test_allies = [Kiro]
+test_enemies = [Generic_Enemy, Bee]
+
 #Readiness Stages
 #a = act
 #b = backswing
 #r = ready
 
-test_start_readiness = [1000, 'b']
+
 
 
 global_participant_readiness = []
 
-def battle_Readiness_Init(Participants, Test_Start_Readiness):
+def battle_Readiness_Init(Participants):
+    """
+    Creates a list of list-pairs of particpants and their readiness, with their readiness taking default values.
+    """
     for entry in Participants:
-        global_participant_readiness.append([entry, Test_Start_Readiness])
+        global_participant_readiness.append([entry, [1000, 'b']])
     return global_participant_readiness
 
 
@@ -54,17 +60,82 @@ def battle_Orderer(Participants_Readiness):
 
 
 def get_next_turn(finish_list):
+    """
+    Figures out who will be acting next (next participant to become ready, excluding those waiting at ready).  Advances time until that participant is ready 
+    """
     for entry in finish_list:
         if entry[2] != 'r':#ignore waiting characters
-            time_advance = entry[1]
-            for participant in global_participant_readiness:
-                participant[1][0] = participant[1][0]-time_advance*participant[0]['speed']#advance participants towards being ready
+            time_advance(entry[1])
             print entry[0], 'is ready!'
-            return
+            return entry[0]
 
 
 
 
 
+def action_turn_end_clean_up(participant):
+    """
+    Takes the participants name as input.
+    Ends the participants turn, giving him defualt readiness and setting him in backswing.
+    Switches all participants waiting at ready to backswing so they have a new opportunity to act.
+    """
+    #edit this code to do the first part! global_participant_readiness[0].index(participant)
+    for entry in global_participant_readiness:
+        if entry[0]['name'] == participant:
+            entry[1] = [1000, 'b']
+        if entry[1][1] == 'r':
+            entry[1][1] = 'a'
+    return
 
-get_next_turn(battle_Orderer(battle_Readiness_Init(test_Participants, test_start_readiness)))
+
+
+
+def time_advance(n):
+    """
+    Advances time n steps.
+    """
+    i = 0
+    while i < n :
+        time_step()
+        i += 1
+    return
+
+def time_step():
+    """
+    Advances time one step.  Add in more code for damage/healing over time eventually
+    """
+    for participant in global_participant_readiness:
+        participant[1][0] = participant[1][0]-participant[0]['speed']#advance participants towards being ready one step
+    return
+
+
+def battle_run(allies, enemies):
+    """
+    Runs a battle until all enemies or allies are defeated.  Or will
+    """
+    while allies:
+        while enemies:
+            nexts_name = get_next_turn(battle_Orderer(global_participant_readiness))
+            if nexts_name == 'Bee':
+                action_turn_end_clean_up(bee_turn())
+            else:
+                break#temporary while turns don't do anything!
+        break#temporary while turns don't do anything!
+        #print 'Victory!'
+        #return
+    #print 'Defeat!'
+
+
+
+def bee_turn():
+    """
+    Bee does something.
+    Returns the Bee's name
+    """
+    print Bee['name'], 'buzzes around!'
+    return Bee['name']
+
+battle_Readiness_Init(test_Participants)
+battle_run(test_allies, test_enemies)
+
+
